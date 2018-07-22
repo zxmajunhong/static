a54f728b38173a66786f0ad336c21c2ffb1b98f7
majunhong <569847911@qq.com>
2018-07-22 18:57:59 +0800

    1

--- js/xiaofeixia.js
+++ js/xiaofeixia.js
@@ -13,5 +13,49 @@ $(function () {
     $('.video-modal .modal').hide();
   });
 
-  $(".area2 .slide").slide({mainCell:".bd ul",autoPlay:true,effect: 'fold',interTime: 3500});
+  // 创建视频播放层方法
+  function showVideo(source) {
+    
+    var html = '<div class="video-layer"><div class="layer"></div>' + 
+                  '<video class="p-pagevideo" autoplay="autoplay">' +
+                    '<source type="video/webm" src="' + source + '">' +
+                    '<source type="video/mp4" src="' + source + '">' +
+                  '</video>' +
+                  '<a href="javascript:;" class="close"></a>' +
+                '</div>';
+    $('body').append(html);
+    $('video').attr('autoplay','autoplay');
+  }
+
+  // 视频关闭事件
+  $('body').on('click', '.layer, .video-layer .close', function () {
+    $('.layer').remove();
+    $('.video-layer').remove();
+  });
+
+  // 通用显示视频方法
+  $('.J_showVideo').click(function () {
+    var videoSrc = $(this).attr('source');
+    showVideo(videoSrc);
+  });
+
+
+  $(".area3 .slide").slide({mainCell:".bd ul",autoPlay:true,effect: 'fold',interTime: 3500});
+
+  // 第四个区域的视频播放
+  $('.area4 .video-list').on('mouseover mouseout', 'li', function (e) {
+    if (e.type == 'mouseover') {
+      $(this).addClass('act').siblings().removeClass('act');
+      $(this).find('.video').trigger('play');
+    } else {
+      $(this).removeClass('act');
+    }
+  });
+
+  // 数字显示
+  $('.ele-list li .num').countUp({time: 1000});
+
+  if (window.getComputedStyle) {
+    new WOW().init(); // IE9+动画效果
+  } // 锚点动画效果
 });
